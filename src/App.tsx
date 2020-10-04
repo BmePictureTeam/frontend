f0f52c77fcc6904a2cee5b8c62439ffc9c2bdd0c
david <ocsai.david@gmail.com>
2020-10-04 21:35:05 +0200

    Frontend alapok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface Instrument {
+  name: string;
+  id: number;
+  family: string;
+  imageUrl: string;
+}
+
+
+interface InstrumentsResponse {
+  instruments: Instrument[];
+
+}
+
 function App() {
+
+  const [instrumentsResponse, updateInstruments] = useState<InstrumentsResponse | undefined>(undefined );
+
+  useEffect(() => {
+    getInstruments();
+  }, []);
+
+  const getInstruments = async () => {
+
+    const result = await fetch("https://temalab.cicum.icu/instruments", {
+      method: "GET",
+    });
+
+    updateInstruments(await result.json())
+
+  };
+ 
+  // let content: any;
+  // if (typeof instrumentsResponse === "undefined") {
+  //   content = "";
+  // } else {
+  //   const instrumentDivs: any[] = [];
+  //   for (const instrument of instrumentsResponse.instruments) {
+  //     instrumentDivs.push(<div>{instrument.name} </div>)
+  //   }
+  //   content = instrumentDivs;
+  // }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {typeof instrumentsResponse === "undefined" ? "" : instrumentsResponse!.instruments.map((instrument) => (<InstrumentView instrument={instrument}  /> ))}
       </header>
     </div>
   );
+}
+
+const InstrumentView  = (props: {instrument: Instrument } ) => {
+return (<div><div className="name"> {props.instrument.name}</div><img src={props.instrument.imageUrl
+} /> </div>);
 }
 
 export default App;
