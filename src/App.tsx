edbb4f5ee6edf6b4aee2007bccde18c5f4a998d1
Ferenc Tamás <tamasf97@outlook.com>
2020-10-10 00:07:42 +0200

    chore: formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 interface Instrument {
   name: string;
@@ -9,30 +9,27 @@ interface Instrument {
   imageUrl: string;
 }
 
-
 interface InstrumentsResponse {
   instruments: Instrument[];
-
 }
 
 function App() {
-
-  const [instrumentsResponse, updateInstruments] = useState<InstrumentsResponse | undefined>(undefined );
+  const [instrumentsResponse, updateInstruments] = useState<
+    InstrumentsResponse | undefined
+  >(undefined);
 
   useEffect(() => {
     getInstruments();
   }, []);
 
   const getInstruments = async () => {
-
     const result = await fetch("https://temalab.cicum.icu/instruments", {
       method: "GET",
     });
 
-    updateInstruments(await result.json())
-
+    updateInstruments(await result.json());
   };
- 
+
   // let content: any;
   // if (typeof instrumentsResponse === "undefined") {
   //   content = "";
@@ -44,19 +41,26 @@ function App() {
   //   content = instrumentDivs;
   // }
 
-
   return (
     <div className="App">
       <header className="App-header">
-        {typeof instrumentsResponse === "undefined" ? "" : instrumentsResponse!.instruments.map((instrument) => (<InstrumentView instrument={instrument}  /> ))}
+        {typeof instrumentsResponse === "undefined"
+          ? ""
+          : instrumentsResponse!.instruments.map(instrument => (
+              <InstrumentView instrument={instrument} />
+            ))}
       </header>
     </div>
   );
 }
 
-const InstrumentView  = (props: {instrument: Instrument } ) => {
-return (<div><div className="name"> {props.instrument.name}</div><img className="kep" src={props.instrument.imageUrl
-} /> </div>);
-}
+const InstrumentView = (props: { instrument: Instrument }) => {
+  return (
+    <div>
+      <div className="name"> {props.instrument.name}</div>
+      <img className="kep" src={props.instrument.imageUrl} />
+    </div>
+  );
+};
 
 export default App;
